9f709b273cb8d15717345490c7085ada04f15c60
Mateusz Leśko <mateusz.lesko@op.pl>
2019-05-18 10:11:14 +0200

    Update create_post.php

--- scripts/CRUD_posts/create_post.php
+++ scripts/CRUD_posts/create_post.php
@@ -1,12 +1,15 @@
 <?php
-echo(file_exists('C:\xampp\htdocs\blog\scripts\database_scripts\connection.php'));
-include 'C:\xampp\htdocs\blog\scripts\database_scripts\connection.php';
+require 'C:\xampp\htdocs\blog\scripts\database_scripts\connection.php';
 
+function create_post(){
 echo($_POST['title'].' created');
 $title = $_POST['title'];
 $content = $_POST['content'];
 
-$pdo = new PDO('mysql:host=localhost;dbname=blog_service;','creator','');
+$connection = new ConnectionDB();
+
+
+$pdo = $connection->connectDB('mysql:host=localhost;dbname=blog_service;','creator','');
 
 $sql_insert = "Insert into posts(IDUser,title,content,upload,IdCategory) values(1,:title,:content,:date,1)";
 $smtl = $pdo->prepare($sql_insert);
@@ -17,5 +20,7 @@ $smtl->execute(array(
 )   
 );
 $pdo->exec($sql_insert);
+}
+create_post();
 
 ?>
