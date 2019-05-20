1117b501b938fa4492ca76873f2b83be3c0aecd2
Mateusz Leśko <mateusz.lesko@op.pl>
2019-05-20 13:18:08 +0200

    CRUD forms and sign up form
    adding a forms

---
+++ blog/scripts/templates/sign_up/sign_up_form.html.php
@@ -0,0 +1,25 @@
+<!Doctype <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <form action="\blog\scripts\sign_up\create_user.php" method='POST'>
+    <label>Nickname</label><br>
+    <input type='text' name='nick' requried>
+    <br>
+    <label>Email</label><br>
+    <input type='text' name='email' requried>
+    <br>
+    <label>Password</label><br>
+    <input type='password' name='psswrd' requried>
+    <br>
+    <input type='submit' value='sign up'>
+    </form>
+</body>
+</html>
